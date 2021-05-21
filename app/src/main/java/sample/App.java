95ed827744d17e62b49a26f0601c7b099766d58c
MC <>
2021-05-21 12:27:52 -0700

    Create map view class

--- app/src/main/java/sample/App.java
+++ app/src/main/java/sample/App.java
@@ -1,10 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
+
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -12,7 +18,10 @@ public class App extends Application {
   private final int START_WIDTH  = 960;
   private final int START_HEIGHT = 540;
 
-  private StackPane root;
+  //private StackPane root;
+  private AnchorPane root;
+
+  MapView map;
 
   @Override
   public void init () throws Exception {
@@ -23,10 +32,20 @@ public class App extends Application {
 
   @Override
   public void start (Stage stage) throws Exception {
-    Button btn = new Button();
-    btn.setText("Say 'Hello World'");
-    root = new StackPane();
-    root.getChildren().add(btn);
+
+    // Create map view
+    map = new MapView();
+
+    //root = new StackPane();
+    //root.getChildren().add(map);
+
+    root = new AnchorPane();
+    root.getChildren().add(map);
+    root.setTopAnchor(map, 0.0);
+    root.setLeftAnchor(map, 0.0);
+    root.setBottomAnchor(map, 0.0);
+    root.setRightAnchor(map, 0.0);
+
     stage.setTitle("Track Commander");
     stage.setScene(new Scene(root, START_WIDTH, START_HEIGHT));
     stage.show();
